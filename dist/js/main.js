edfdaa85fc394a1d72f347dc8e9916b3b6cc8e2e
michellejames <shellyjames90@yahoo.com>
2021-07-14 19:10:06 -0400

    Accessibility Demo

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,9 @@
 "use strict";
 
 console.log("Hello World from main.js! \nChange this message, and make sure it changes in the browser \nto verify that you're working in the right files.");
+var customCheckbox = document.getElementById('square');
+customCheckbox.addEventListener('click', function (event) {
+  customCheckbox.style.borderColor = "#24bf41";
+  customCheckbox.style.backgroundColor = "#24bf41";
+});
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,12 @@
 console.log(`Hello World from main.js! 
 Change this message, and make sure it changes in the browser 
 to verify that you're working in the right files.`)
+
+const customCheckbox = document.getElementById('square');
+
+customCheckbox.addEventListener('click', event => {
+   
+    customCheckbox.style.borderColor="#24bf41";
+    customCheckbox.style.backgroundColor="#24bf41";
+
+});
